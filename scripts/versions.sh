c9f612e40efd5342135f647516012a910fc4143b
alexyk <alexander.kostadinov@locktrip.com>
2019-01-16 12:13:51 +0200

    Fix Travis CI build and improve: - Use Xcode workspace file instead (.xcworkspace) - Set specific node version 8.14.1 (comes with npm 6.4.1) - Move versions output to "./scripts/versions.sh"

---
+++ scripts/versions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ @echo "======= START::Versions List::START ========"
+ uname -a
+ brew --version
+ xcodebuild -version
+ java -version
+#  node --version
+#  npm -version
+@echo "========= END::Versions List::END =========="
