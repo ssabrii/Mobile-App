68eb868001108f90d1c061ad1e7f1c215d39729b
alexyk <alexander.kostadinov@locktrip.com>
2019-01-25 11:13:08 +0200

    Enable features and android debugging

--- src/utils/reactotronLogging.js
+++ src/utils/reactotronLogging.js
@@ -2,16 +2,17 @@ import Reactotron, {openInEditor} from 'reactotron-react-native'
 
 Reactotron
   .configure({
-    name: "React Native Demo"
+    name: "React Native Demo",
+    host: "localhost"
   })
   .useReactNative({
-    asyncStorage: false, // there are more options to the async storage.
-    networking: { // optionally, you can turn it off with false.
-      ignoreUrls: /symbolicate/
-    },
-    editor: false, // there are more options to editor
-    errors: { veto: (stackFrame) => false }, // or turn it off with false
-    overlay: false, // just turning off overlay
+    // asyncStorage: false, // there are more options to the async storage.
+    // networking: { // optionally, you can turn it off with false.
+    //   ignoreUrls: /symbolicate/
+    // },
+    // editor: false, // there are more options to editor
+    // errors: { veto: (stackFrame) => false }, // or turn it off with false
+    // overlay: false, // just turning off overlay
   })
   .use(openInEditor())
   .connect()
