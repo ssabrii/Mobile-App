61cefb687c8739cfea704fb5a6a6ae71b274a9ec
alexyk <alexander.kostadinov@locktrip.com>
2019-02-19 17:29:27 +0200

    Fix build

---
+++ scripts/travis_version.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+job=$1
+branch=$2
+sed "s/const\ travisVersion.*/const\ travisVersion='$job';"  src/version.js > src/version.js
+sed "s/const\ branchName.*/const\ branchName='$branch';"  src/version.js > src/version.js
