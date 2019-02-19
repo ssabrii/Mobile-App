934f49dafaae94ca102ab96382ce16e27fc3b12c
alexyk <alexander.kostadinov@locktrip.com>
2019-02-19 18:21:23 +0200

    Try travis build  without "sed" in travis_version.sh

--- scripts/travis_version.sh
+++ scripts/travis_version.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 job=$1
 branch=$2
-sed "s/const\ travisVersion.*/const\ travisVersion='$job';"  src/version.js > src/version.js
-sed "s/const\ branchName.*/const\ branchName='$branch';"  src/version.js > src/version.js
+# sed "s/const\ travisVersion.*/const\ travisVersion='$job';"  src/version.js > src/version.js
+# sed "s/const\ branchName.*/const\ branchName='$branch';"  src/version.js > src/version.js
+echo "Job: $job"
+echo "Branch: $branch"
